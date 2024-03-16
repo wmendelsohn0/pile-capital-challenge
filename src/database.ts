e0ea8631f46c66b1edc1b18f192d534e8f10abd9
wmendelsohn0 <wmendelsohn0@gmail.com>
2024-03-16 17:07:41 +0000

    A few notes

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,7 @@
 import {Pool} from "pg";
 
+// In a real application, these would be environment variables, potentially loaded from a .env file
+
 export const createPool = (): Pool => {
     return new Pool({
         user: "pile_user",
